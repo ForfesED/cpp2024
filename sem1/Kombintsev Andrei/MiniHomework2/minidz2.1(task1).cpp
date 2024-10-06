a085c66bf301c9146445868f94f4c9cb6f4946ea
Andrei Kombintsev <forfesed@gmail.com>
2024-10-06 22:19:41 +0300

    Kombintsev Andrei

---
+++ sem1/Kombintsev Andrei/MiniHomework2/minidz2.1(task1).cpp
@@ -0,0 +1,56 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+
+
+int notationeighteen(int number) {
+    std::string minus =("");
+    
+    if (number < 0) { 
+         minus.append ("-");
+        number = number * (-1);
+    }
+    std::cout << minus << std::hex << number << '\n'; //уффф читыыы эта штука мсама переводит из 10 в 16 сист, такая же есть для 8 сист, но это уже слишком
+    return 0;
+}
+int  notationeight(int number) {
+    std::string minus =("");
+
+    if (number < 0) {
+        minus.append("-");
+        number = number * (-1);
+    }
+    int numbL = 0;//переменная для ответа тут огн будет храниться
+    int k = 1; //разряд числа
+    while (number != 0) {
+
+        numbL += number % 8 * k;
+        number = number / 8;
+        k *= 10;
+    }
+    std::cout << minus << numbL;
+    return 0;
+}
+
+int main()
+{
+    int numbchoose = 0;
+    int number = 0;
+    std::cout << "Enter the number to convert to 16 or 8 number system:\n";
+    std::cin >> number;
+    std::cout << "\n";
+    std::cout << "To which number system do you want to convert the number 0 - 8th or 1 - 16th\n";
+    std::cin >> numbchoose;
+    if (numbchoose == 0) {
+       int answer =notationeight(number);
+       
+    }
+    else if (numbchoose == 1) {
+      int answer =notationeighteen(number);
+        
+    }
+    else { std::cout << "ERROR TRY  GAIN\n"; }
+    
+}
---
+++ sem1/Kombintsev Andrei/MiniHomework2/minidz2.2(task2).cpp
@@ -0,0 +1,28 @@
+﻿// //ГИПОТЕЗА СИРАКУЗ
+//
+
+#include <iostream>
+
+int main()
+{
+    int long long number = 0;
+    std::cout << "Enter your number > 0\n";
+    std::cin >> number;
+    std::cout << "\n";
+    if (number < 1) { std::cout << "EROOR\n"; }
+    else {
+        while (number != 1) {
+            if (number % 2 == 0) { number /= 2; \
+                std::cout << number << " Action %2==0\n";
+            }
+            else { number = (number * 3 + 1) / 2; 
+            std::cout << number << " Action %2!=0\n";
+            }
+
+        }
+
+        std::cout << number << "\n";
+    }
+
+}
+
